c6ed3830ea54c30ecfced3e6d3e25535fd3e7ecc
Cary Price <cary@vipmail.com>
2020-05-04 14:56:06 +0900

    Post trng session checkin. Good class.

--- src/views/Attendees.vue
+++ src/views/Attendees.vue
@@ -1,44 +1,42 @@
 <template>
   <div class="container mt-4">
+    <div
+  class="row justify-content-center"
+>
+  <div class="col-md-8" v-if="user !== null && user.uid == userID">
+    <h1 class="font-weight-light text-center">Attendees</h1>
 
-    <div class="row justify-content-center">
-      <div
-        class="col-md-8"
-        v-if="user !== null && user.uid == userID"
-      >
-        <h1 class="font-weight-light text-center">Attendees</h1>
-
-        <div class="card bg-light mb-4">
-          <div class="card-body text-center">
-            <div class="input-group input-group-lg">
-              <input
-                type="text"
-                placeholder="Search Attendees"
-                class="form-control"
-                v-model="searchQuery"
-                ref="searchQuery"
-              />
-              <div class="input-group-append">
-                <button
-                  class="btn btn-sm btn-outline-info"
-                  title="Pick a random attendee"
-                  @click="chooseRandom"
-                >
-                  <font-awesome-icon icon="random"></font-awesome-icon>
-                </button>
-                <button
-                  class="btn btn-sm btn-outline-info"
-                  title="Reset Search"
-                  @click="resetQuery"
-                >
-                  <font-awesome-icon icon="undo"></font-awesome-icon>
-                </button>
-              </div>
-            </div>
+    <div class="card bg-light mb-4">
+      <div class="card-body text-center">
+        <div class="input-group input-group-lg">
+          <input
+            type="text"
+            placeholder="Search Attendees"
+            class="form-control"
+            v-model="searchQuery"
+            ref="searchQuery"
+          />
+          <div class="input-group-append">
+            <button
+              class="btn btn-sm btn-outline-info"
+              title="Pick a random attendee"
+              @click="chooseRandom"
+            >
+              <font-awesome-icon icon="random"></font-awesome-icon>
+            </button>
+            <button
+              class="btn btn-sm btn-outline-info"
+              title="Reset Search"
+              @click="resetQuery"
+            >
+              <font-awesome-icon icon="undo"></font-awesome-icon>
+            </button>
           </div>
         </div>
       </div>
     </div>
+  </div>
+</div>
     <div class="row justify-content-center">
       <div
         class="col-8 col-sm-6 col-md-4 col-lg-3 mb-2 p-0 px-1"
@@ -104,9 +102,7 @@ export default {
   },
   computed: {
     filteredAttendees: function() {
-      const dataFilter = item =>
-        item.displayName.toLowerCase().match(this.searchQuery.toLowerCase()) &&
-        true;
+      const dataFilter = item => item.displayName.toLowerCase().match(this.searchQuery.toLowerCase()) && true;
       return this.displayAttendees.filter(dataFilter);
     }
   },
